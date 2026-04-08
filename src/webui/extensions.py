e6e3e354382061c0ce7d9e09d859f64897cc2a65
lolovlad <lolovlad39@gmail.com>
2026-04-08 13:46:39 +0500

    v 1.7 valid date

--- src/webui/extensions.py
+++ src/webui/extensions.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
 from flask_login import LoginManager
-from flask_socketio import SocketIO
 from flask_session import Session
 from flask_wtf import CSRFProtect
 
 csrf = CSRFProtect()
 server_session = Session()
 login_manager = LoginManager()
-socketio = SocketIO(async_mode="threading", cors_allowed_origins="*")
